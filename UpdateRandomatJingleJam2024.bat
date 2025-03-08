03df1441d85867c43c5d35edb5c9f71ffa1f53c9
Malivil <mlebson@outlook.com>
2025-03-08 10:01:02 -0500

    Initial publish

--- UpdateRandomatJingleJam2024.bat
+++ UpdateRandomatJingleJam2024.bat
@@ -1,4 +1,4 @@
 @echo off
 set /p changes="Enter Changes: "
-"C:\Program Files (x86)\Steam\steamapps\common\GarrysMod\bin\gmpublish.exe"  update -addon "TTT-Randomat-20-JingleJam2024.gma" -id "TODO" -changes "%changes%"
+"C:\Program Files (x86)\Steam\steamapps\common\GarrysMod\bin\gmpublish.exe"  update -addon "TTT-Randomat-20-JingleJam2024.gma" -id "3440665808" -changes "%changes%"
 pause
